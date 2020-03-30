78c18d155ba4cf5ec2aff39a592b8391bead9a64
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-03-30 13:42:16 +0200

    Updates

---
+++ precache-manifest.bec657f4149041642694e42bfb100b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b115d14995a70dfc9991988f817d80",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "3443b0f0d27b876a2d85",
+    "url": "/TodoList/static/css/main.68f8febf.chunk.css"
+  },
+  {
+    "revision": "34fb6c35e4291649e002",
+    "url": "/TodoList/static/js/2.773537d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.773537d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3443b0f0d27b876a2d85",
+    "url": "/TodoList/static/js/main.5035e9f8.chunk.js"
+  },
+  {
+    "revision": "d40a4c52ed2ed66c0472",
+    "url": "/TodoList/static/js/runtime-main.b78541ab.js"
+  }
+]);
